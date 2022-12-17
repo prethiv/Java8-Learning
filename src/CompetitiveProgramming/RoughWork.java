8a37202cd513d639057e054816ef49695c3dbe95
NITHISHGM <nithishkumars@geakminds.com>
2022-12-17 14:30:12 +0530

    initial update

--- src/CompetitiveProgramming/RoughWork.java
+++ src/CompetitiveProgramming/RoughWork.java
@@ -4,5 +4,6 @@ public class RoughWork {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Hello Nithish");
+        System.out.println("hello jalz");
     }
 }
