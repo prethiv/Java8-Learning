0c5266e90b179693fe93b404ed006ae886cc7a71
prethiv <prethiv191@gmail.com>
2022-12-17 15:04:10 +0530

    Comments basics

---
+++ src/CompetitiveProgramming/Basics/comments.java
@@ -0,0 +1,15 @@
+package CompetitiveProgramming.Basics;
+
+public class comments {
+    public static void main(String[] args) {
+        /*System.out.println("hello comments");
+        System.out.println("hello comments");
+        System.out.println("hello comments");
+        This is a multi line comment
+        */
+
+//        System.out.println("Love");
+//   Single line comment
+        System.out.println("hello comments");
+    }
+}
