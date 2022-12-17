60a3d20d3118b367cd11f7c06a3d2690db1a7126
prethiv <prethiv191@gmail.com>
2022-12-17 14:26:44 +0530

    Second line

--- src/CompetitiveProgramming/RoughWork.java
+++ src/CompetitiveProgramming/RoughWork.java
@@ -3,5 +3,6 @@ package CompetitiveProgramming;
 public class RoughWork {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello Nithish");
     }
 }
