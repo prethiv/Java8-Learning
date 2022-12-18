e0e7d4855f64ad6b6ececf3ea20e0ab6c7b064c3
prethiv <prethiv191@gmail.com>
2022-12-18 16:27:25 +0530

    BST Construct

--- src/CompetitiveProgramming/PrethivRoughWork/BstConstruct.java
+++ src/CompetitiveProgramming/PrethivRoughWork/BstConstruct.java
@@ -1,6 +1,8 @@
 package CompetitiveProgramming.PrethivRoughWork;
 
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class BstConstruct {
@@ -31,6 +33,42 @@ public class BstConstruct {
         }
     }
 
+    public static void lvlorder(Node root){
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        System.out.println();
+        while(!q.isEmpty()){
+
+            Node temp = q.poll();
+            System.out.println(temp.key+" ");
+            if(temp.left!=null){
+                q.add(temp.left);
+            }
+            if(temp.right!=null){
+                q.add(temp.right);
+            }
+
+
+        }
+        System.out.println();
+    }
+
+    public static int heightOfTheTree(Node root){
+        if(root==null){
+            return 0;
+        }
+        else{
+            int left = heightOfTheTree(root.left);
+            int right = heightOfTheTree(root.right);
+            if(left>right){
+                return left+1;
+            }
+            else{
+                return right+1;
+            }
+        }
+    }
+
     public static void main(String[] args) {
 
         Node root =new Node(1);
@@ -50,6 +88,8 @@ public class BstConstruct {
             root1=insertIntoTree(root1,temp);
         }
         inorder(root1);
+        lvlorder(root1);
+        System.out.println("The height of the given tree is "+heightOfTheTree(root1));
 
 
     }
