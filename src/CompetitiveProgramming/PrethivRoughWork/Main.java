ff278e80a2febe146caed6883ba341b51c4bd918
prethiv <prethiv191@gmail.com>
2022-12-25 19:32:29 +0530

    CP Basics 25-12-2022

--- src/CompetitiveProgramming/PrethivRoughWork/Main.java
+++ src/CompetitiveProgramming/PrethivRoughWork/Main.java
@@ -1,25 +1,36 @@
 package CompetitiveProgramming.PrethivRoughWork;
-import java.util.*;
-import java.util.stream.Collectors;
+
+
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main {
 
-
     public static void main(String[] args) {
-        Scanner scanner= new Scanner(System.in);
-        String firstLine  = scanner.nextLine();
-        List<Integer> list = Arrays.stream(scanner.nextLine().split(" ")).map(item->{
-            return Integer.parseInt(item);
-        }).collect(Collectors.toList());
-        Integer k = Integer.parseInt(firstLine.split(" ")[1]);
-        String result="";
-        for(Integer data:list){
-            if(data!=k){
-                result+=data;
-                result+=" ";
+
+        Scanner scanner = new Scanner(System.in);
+
+        Double days = scanner.nextDouble();
+        scanner.nextLine();
+        HashMap<Character,Double> attendence = new HashMap<>();
+        for(int i=0;i<days;i++){
+            Character PorA = scanner.next().toCharArray()[0];
+            try {
+                attendence.put(PorA, attendence.get(PorA) + 1.0);
+            }
+            catch(Exception e){
+                attendence.put(PorA,1.0);
             }
         }
-        result=result.trim().length()==0?"empty":result.trim();
-        System.out.println(result);
+
+        Double percentage = Double.valueOf((days/attendence.get('P'))*100);
+
+        if(percentage>25){
+            System.out.print("Not Blacklisted");
+        }
+        else{
+            System.out.print("Blacklisted");
+        }
+
     }
 }
